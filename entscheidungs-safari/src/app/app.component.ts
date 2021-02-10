d390bf4feea98dde8d78913c3d87710544471222
haeul <lea.haeussermann@akka.eu>
2021-02-11 00:09:43 +0100

    changes

--- entscheidungs-safari/src/app/app.component.ts
+++ entscheidungs-safari/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {NavigationEnd, Router} from '@angular/router';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'entscheidungs-safari';
   isCollapsed = true;
   constructor(public translateService: TranslateService, public router: Router) {
@@ -17,9 +18,11 @@ export class AppComponent {
       }
       window.scrollTo(0, 0);
     });
+
   }
 
   refresh() {
     this.router.navigate(['/home']);
   }
+
 }
